d91f8155888045e2626fbfab79528e1960cc9d3a
Alexandra Janin <contact.ajanin@gmail.com>
2018-06-26 14:46:43 +0200

    Disabled localstorage

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
-import persistState from 'redux-localstorage';
+// import persistState from 'redux-localstorage';
 import api from 'middlewares/api';
 import rootReducer from 'root-reducer';
 
@@ -37,7 +37,7 @@ const composedEnhancers = compose(
 export const store = createStore(
   rootReducer,
   composedEnhancers,
-  persistState(['userrequest', 'forms']),
+  // persistState(['userrequest', 'forms']),
 );
 
 export default store;
