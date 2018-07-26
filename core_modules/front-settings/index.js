504482db59bd70a8caff78d5f2906ba50e934dc6
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-07-26 11:29:10 +0200

    Enable variables injection from `injection.js`

--- core_modules/front-settings/index.js
+++ core_modules/front-settings/index.js
@@ -10,13 +10,20 @@ const defaultSettings = {
   TIME_TO_STALE: 300000, // 5 min
 };
 
-/**
- * Generate envSettings from ENV variables prefixed by REACT_APP_
- */
-const envSettings = Object.keys(process.env).reduce((acc, curr) => {
+const prefixFiltering = scope => (acc, curr) => {
   const [, prefix, key] = /(.{10})(.*)/.exec(curr) || [];
-  return prefix === 'REACT_APP_' ? { ...acc, [key]: process.env[curr] } : acc;
-}, {});
+  return prefix === 'REACT_APP_' ? { ...acc, [key]: scope[curr] } : acc;
+};
+
+/**
+ * Generate envSettings from `process.env` variables prefixed by `REACT_APP_`
+ */
+const envSettings = Object.keys(process.env).reduce(prefixFiltering(process.env), {});
+
+/**
+ * Generate injectedSettings from `window` variables prefixed by `REACT_APP_`
+ */
+const injectedSettings = Object.getOwnPropertyNames(window).reduce(prefixFiltering(window), {});
 
 /**
  * Merge both objects
@@ -24,4 +31,5 @@ const envSettings = Object.keys(process.env).reduce((acc, curr) => {
 export default {
   ...defaultSettings,
   ...envSettings,
+  ...injectedSettings,
 };
