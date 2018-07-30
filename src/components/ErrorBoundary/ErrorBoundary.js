0330a7839d064d32a89b81823075363d5cc34e16
Alexandra Janin <contact.ajanin@gmail.com>
2018-07-30 15:31:25 +0200

    Add ErrorBoundary component

---
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Alert } from 'antd';
+
+class ErrorBoundary extends React.Component {
+  state = { hasError: false };
+
+  componentDidCatch () {
+    // Display fallback UI
+    this.setState({ hasError: true });
+    // TODO: Set up and call an error reporting service
+  }
+
+  render () {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <Alert
+          style={this.props.style || { margin: 10 }}
+          description={this.props.message || 'Something went wrong.'}
+          type="error"
+        />
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
