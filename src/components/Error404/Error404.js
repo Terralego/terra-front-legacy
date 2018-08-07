68083f5dc3d7c0c8df51b53803269d51ca9b42be
Aymeric Guillien <aymericguillien@gmail.com>
2018-08-07 09:56:42 +0200

    Add link to home page in 404 page

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Icon } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Error404 = () => (
   <div>
-    Erreur 404 : Page introuvable.
+    <h1>Erreur 404 : Page introuvable.</h1>
+    <p><strong>Retourner à la page d&#39;accueil: </strong>
+      <Link to="/">
+        <Icon type="home" />
+        Accueil
+      </Link>
+    </p>
   </div>
 );
 
