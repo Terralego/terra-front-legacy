9773a9bb509fd31acbc8119b969e53f373b4dec6
Alexandra Janin <contact.ajanin@gmail.com>
2018-08-20 17:49:16 +0200

    Set i18n debug to false

--- src/i18n.js
+++ src/i18n.js
@@ -34,7 +34,7 @@ i18n
     // we init with resources
     resources: { en, fr },
     fallbackLng: 'en',
-    debug: true,
+    debug: false,
 
     // have a common namespace used around the full app
     nsMode: 'default',
