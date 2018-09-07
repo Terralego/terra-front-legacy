4bb105d0019d43442646268a3461d74b067ea9c7
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-09-07 11:53:15 +0200

    Convert Drawer to functional component & improve propTypes

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -5,44 +5,40 @@ import { Button, Icon } from 'antd';
 
 import styles from 'components/Drawer/Drawer.module.scss';
 
-class Drawer extends React.Component {
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    visible: PropTypes.bool,
-  };
-
-  static defaultProps = {
-    visible: false,
-  };
-
-  render () {
-    const { visible } = this.props;
-
-    return (
-      <div
+const Drawer = ({ visible, id, children, handleVisibilityToggle }) => (
+  <div
+    className={classnames({
+      [styles.drawer]: true,
+      [styles['drawer--expanded']]: visible,
+      [`drawer__${id}--expanded`]: visible,
+    })}
+    id={id}
+  >
+    {children}
+    {handleVisibilityToggle &&
+      <Button
         className={classnames({
-          [styles.drawer]: true,
-          [styles['drawer--expanded']]: visible,
-          [`drawer__${this.props.id}--expanded`]: visible,
+          [styles.collapseButton]: true,
+          [styles['collapseButton--visible']]: visible,
         })}
-        id={this.props.id}
+        onClick={handleVisibilityToggle}
+        aria-controls={id}
+        aria-expanded={visible}
       >
-        {this.props.children}
-        <Button
-          className={classnames({
-            [styles.collapseButton]: true,
-            [styles['collapseButton--visible']]: visible,
-          })}
-          onClick={this.props.handleVisibilityToggle}
-          aria-controls={this.props.id}
-          aria-expanded={visible}
-        >
-          {visible && 'Replier le panneau'}
-          <Icon className={styles.collapseIcon} type="double-right" />
-        </Button>
-      </div>
-    );
-  }
-}
+        {visible && 'Replier le panneau'}
+        <Icon className={styles.collapseIcon} type="double-right" />
+      </Button>}
+  </div>
+);
+
+Drawer.propTypes = {
+  id: PropTypes.string.isRequired,
+  visible: PropTypes.bool,
+  handleVisibilityToggle: PropTypes.func,
+};
+
+Drawer.defaultProps = {
+  visible: false,
+};
 
 export default Drawer;
