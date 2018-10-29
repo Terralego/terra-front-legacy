55f1a0f145ab897f32d994f9bc1ea4c81c70bb69
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-10-29 14:10:45 +0100

    Add js-cookie dependency & store JWT also in cookie
    	Refs: Terralego/Customers/onf-front#461

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -1,14 +1,23 @@
+import Cookies from 'js-cookie';
+
+const cookieOptions = {
+  // domain: '.makina-corpus.net',
+};
+
 class TokenService {
   constructor () {
     this.token = localStorage.getItem('token') || null;
+    // this.token = Cookies.get('jwt') || null;
   }
   setToken (token) {
     this.token = token;
     localStorage.setItem('token', token);
+    Cookies.set('jwt', token, cookieOptions);
   }
   removeToken () {
     this.token = null;
     localStorage.removeItem('token');
+    Cookies.remove('jwt', cookieOptions);
   }
   getToken () {
     return this.token;
